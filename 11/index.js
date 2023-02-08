34e3bce3c17bbf576ad1ba36a76a558385c5bda0
rakib417 <rakib417@gmail.com>
2023-02-08 16:00:13 +0600

    we

--- 11/index.js
+++ 11/index.js
@@ -1,1 +1,17 @@
+for(let i=0; i<10; i++){
+
 console.log("hello world");
+}
+
+
+for(let i=5; i<51; i = i+5){
+    console.log(i);
+}
+
+for(let i=10; i<101; i = i+10){
+    console.log(i);
+}
+
+for(let i=3; i<90; i = i+3){
+    console.log(i);
+}
